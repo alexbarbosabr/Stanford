f3c05cf48f4f37c0d494f5d844f053daa5bff665
Alex Barbosa <alex.barbosa.br@gmail.com>
2024-03-11 19:11:39 -0300

    Lecture 1 and 2

---
+++ CS193p2023/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Alex on 06/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var emojis: [String] = ["🎃", "👻", "💀", "🕷️", "🕯️", "🧹", "☠️", "😈", "🧙‍♀️", "🕸️", "😱", "🍭", "🙀", "🍬"]
+    
+    @State var cardCount = 4
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                cards.padding()
+            }
+            Spacer()
+            cardCountAdjusters
+                .padding()
+        }
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { emoji in
+                CardView(content: emojis[emoji])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol:  "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content)
+                    .font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CS193p2023/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alex on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
