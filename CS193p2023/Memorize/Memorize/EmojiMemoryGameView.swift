0fa4c1d86901d6f2fdd494a7aff8e913e040c6d0
Alex Barbosa <alex.barbosa.br@gmail.com>
2024-03-29 20:21:23 -0300

    Lectures 7 to 9

--- CS193p2023/Memorize/Memorize/EmojiMemoryGameView.swift
+++ CS193p2023/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -8,57 +8,111 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-    
+    typealias Card = MemoryGame<String>.Card
     @ObservedObject var viewModel: EmojiMemoryGame
     
     private let aspectRatio: CGFloat = 2/3
+    private let spacing: CGFloat = 4
+    private let dealAnimation: Animation = .easeInOut(duration: 1)
+    private let dealInterval: TimeInterval = 0.15
+    private let deckWidth: CGFloat = 50
     
     var body: some View {
         VStack {
-            cards
-                .animation(.default, value: viewModel.cards)
-            Button("Shuffle") {
-                viewModel.shuffle()
+            cards.foregroundColor(viewModel.color)
+            HStack {
+                score
+                Spacer()
+                deck.foregroundColor(viewModel.color)
+                Spacer()
+                shuffle
+
             }
+            .font(.largeTitle)
         }
         .padding()
     }
     
+    private var score: some View {
+        Text("Score: \(viewModel.score)")
+            .animation(nil)
+    }
+    
+    private var shuffle: some View {
+        Button("Shuffle") {
+            withAnimation {
+                viewModel.shuffle()
+            }
+        }
+    }
+    
     private var cards: some View {
         AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
-            CardView(card)
-                .padding(4)
-                .onTapGesture {
-                    viewModel.choose(card)
-                }
-        }
-        .foregroundColor(.orange)
-    }
-}
-
-struct CardView: View {
-    let card: MemoryGame<String>.Card
-    
-    init(_ card: MemoryGame<String>.Card) {
-        self.card = card
-    }
-    
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(card.content)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.01)
-                    .aspectRatio(1, contentMode: .fit)
+            if isDealt(card) {
+                CardView(card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(.asymmetric(insertion: .identity, removal: .identity))
+                    .padding(spacing)
+                    .overlay(FlyingNumber(number: scoreChange(causeBy: card)))
+                    .zIndex(scoreChange(causeBy: card) != 0 ? 100 : 0)
+                    .onTapGesture {
+                        choose(card)
+                        
+                    }
             }
-                .opacity(card.isFaceUp ? 1 : 0)
-            base.fill()
-                .opacity(card.isFaceUp ? 0 : 1)
         }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+    
+    @State private var dealt = Set<Card.ID>()
+    
+    private func isDealt(_ card: Card) -> Bool {
+        dealt.contains(card.id)
+    }
+    
+    private var undealtCards: [Card] {
+        viewModel.cards.filter { !isDealt($0) }
+    }
+    
+    @Namespace private var dealingNamespace
+    
+    private var deck: some View {
+        ZStack {
+            ForEach(undealtCards) { card in
+                CardView(card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(.asymmetric(insertion: .identity, removal: .identity))
+            }
+            .frame(width: deckWidth, height: deckWidth / aspectRatio)
+            .onTapGesture {
+                deal()
+            }
+        }
+    }
+    
+    private func deal() {
+        var delay: TimeInterval = 0
+        for card in viewModel.cards {
+            withAnimation(dealAnimation.delay(delay)) {
+                _ = dealt.insert(card.id)
+            }
+            delay += dealInterval
+        }
+    }
+    
+    private func choose(_ card: Card) {
+        withAnimation {
+            let scoreBeforeChrossing = viewModel.score
+            viewModel.choose(card)
+            let scoreChange = viewModel.score - scoreBeforeChrossing
+            lastScoreChange = (scoreChange, card.id)
+        }
+    }
+    
+    @State private var lastScoreChange = (0, causedByCardId: "")
+    
+    private func scoreChange(causeBy card: Card) -> Int {
+        let (amount, id) = lastScoreChange
+        return card.id == id ? amount : 0
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Stanford
-Stanford University's course CS193p (Developing Applications for iOS using SwiftUI) 
+# Stanford University's course CS193p 2023
+
+Developing Applications for iOS using SwiftUI.
+
+Teacher Paul Hegarty
+
+[course website](https://cs193p.sites.stanford.edu/2023)
