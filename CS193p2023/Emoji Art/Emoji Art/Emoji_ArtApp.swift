628408a702642b8423b0cb975f420172f5cce681
Alex Barbosa <alex.barbosa.br@gmail.com>
2024-04-20 01:40:58 -0300

    Lecture 15

--- CS193p2023/Emoji Art/Emoji Art/Emoji_ArtApp.swift
+++ CS193p2023/Emoji Art/Emoji Art/Emoji_ArtApp.swift
@@ -9,16 +9,9 @@ import SwiftUI
 
 @main
 struct Emoji_ArtApp: App {
-    @StateObject var defaultDocument = EmojiArtDocument()
-    @StateObject var palleteStore = PaletteStore(named: "Main")
-    @StateObject var palleteStore2 = PaletteStore(named: "Alternate")
-    @StateObject var palleteStore3 = PaletteStore(named: "Special")
-    
     var body: some Scene {
-        WindowGroup {
-            EmojiArtDocumentView(document: defaultDocument)
-//            PaletteManager(stores: [palleteStore, palleteStore2, palleteStore3])
-                .environmentObject(palleteStore)
+        DocumentGroup(newDocument: { EmojiArtDocument() }) { config in
+            EmojiArtDocumentView(document: config.document)
         }
     }
 }
